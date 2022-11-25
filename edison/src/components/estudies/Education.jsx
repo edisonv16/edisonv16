6834a8391463af47cdc5c3689d8380dd3b3282bc
Edison Ospina <edisonv16@hotmail.com>
2022-11-25 00:36:28 -0500

    Education
    Education

--- edison/src/components/estudies/Education.jsx
+++ edison/src/components/estudies/Education.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
+import InfoEducation from '../../data/InfoEducation.jsx';
+
 const Education = () => {
+    const [education, setEducation] = useState(InfoEducation);
+
+
+
     return (
         <div className="row education">
             <div className="three columns header-col">
-                <h1><span>Education</span></h1>
+                <h1><span>Formación Academica, Cursos, Enfasis y Diplmados</span></h1>
             </div>
             <div className="nine columns main-col">
-                <div className="row item">
+                {education.education.map((item, index) => {
+                    return (
+                        <div className="row item" key={index}>
+                            <div className="twelve columns">
+                                <h3>{item.institucion}</h3>
+                                {item.curso.map((item, index) => {
+                                    return (
+                                        <p className="info" key={index}>
+                                            <span>&bull;</span>{item.curso}
+                                        </p>
+                                        )
+                                    })}
+                            </div>
+                        </div>
+                    )
+                })}
+
+                {/* <div className="row item">
                     <div className="twelve columns">
                         <h3>Vivilab bogotá</h3>
                         <p className="info">
@@ -13,42 +37,7 @@ const Education = () => {
                             <br />Taller de Lean y Ux<span>&bull;</span> <em className="date">Noviembre 2014</em>
                         </p>
                     </div>
-                </div>
-                <div className="row item">
-                    <div className="twelve columns">
-                        <h3>oja.la (Ministerio de técnologia)</h3>
-                        <p className="info">Diplomado en Freelance y trabajo 3.0 <span>&bull;</span><em className="date">Diciembre 2015</em>,<br /> Diplomado en desarrollo web <span>&bull;</span><em className="date">Enero -2016  </em> </p>
-                    </div>
-                </div>
-                <div className="row item">
-                    <div className="twelve columns">
-                        <h3>Platzi</h3>
-                        <p className="info">
-                            Curso de diseño de Interfaces y Ux.<span>&bull;</span><em className="date">Noviembre 2015</em>
-                            <br />Curso de diseño de interfaces móviles.<span>&bull;</span><em className="date">Octubre- 2015  </em>
-                            <br />Curso de introducción al diseño de interfaces<span>&bull;</span><em className="date">Septiembre - 2015</em>
-                            <br />Curso de programación básica<span>&bull;</span><em className="date">Septiembre - 2015</em>
-                            <br />Curso de Illustrator<span>&bull;</span><em className="date">Agosto - 2015</em>
-                        </p>
-                    </div>
-                </div>
-                <div className="row item">
-                    <div className="twelve columns">
-                        <h3>Universidad de Artes y Letras</h3>
-                        <p className="info">
-                            Énfasis en diseño y desarrollo web site<span>&bull;</span><em className="date"> 2011</em>
-                            <br />Técnico profesional en Diseño Gráfico<span>&bull;</span><em className="date">Octubre- 2011</em>
-                        </p>
-                    </div>
-                </div>
-                <div className="row item">
-                    <div className="twelve columns">
-                        <h3>Colegio Distrital Jorge Eliécer Gaitán</h3>
-                        <p className="info">
-                            Bachiller Académico<span>&bull;</span><em className="date">Diciembre- 2007</em>
-                        </p>
-                    </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
---
+++ edison/src/data/InfoEducation.jsx
@@ -0,0 +1,166 @@
+const InfoEducation = {
+    education: [
+        {
+            id:1,
+            institucion:'Colsubsidio',
+            curso:[
+                {
+                    id:1,
+                    curso:'Big Data',
+                },
+                {
+                    id:2,
+                    curso:'Ciberseguridad y Scrum',
+                },
+            ],
+        },
+        {
+            id:2,
+            institucion:'CertiProf',
+            curso:[
+                {
+                    id:1,
+                    curso:'Scrum',
+                },
+            ]
+        },
+        {
+            id:3,
+            institucion:'Google',
+            curso:[
+                {
+                    id:1,
+                    curso:'Comercio electrónico',
+                },
+                {
+                    id:2,
+                    curso:'Marketing digital',
+                },
+                {
+                    id:3,
+                    curso:'Protege tu Negocio: Ciberseguridad en el Teletrabajo',
+                }
+            ],
+        },
+        {
+            id:4,
+            institucion:'Asociación de la publicidad, el marketing y la comunicación digital en Europa',
+            curso:[
+                {
+                    id:1,
+                    curso:'Marketing Digital IAB'
+                }
+            ]
+        },
+        {
+            id:5,
+            institucion:'Ciudadania Digital(Ministerio de Tecnología de Colombia)',
+            curso:[
+                {
+                    id:1,
+                    curso:'Fundamentos de Marketing Digital'
+                }
+            ]   
+        },
+        {
+            id:6,
+            institucion:'oja.la(Ministerio de Tecnología de Colombia)',
+            curso:[
+                {
+                    id:1,
+                    curso:'Diplomado en Desarrollo Web',
+                },
+                {
+                    id:2,
+                    curso:'Diplomado en Freelance y trabajo 3.0',
+                },
+                {
+                    id:3,
+                    curso:'Conceptos básicos de Mercadeo Digital',
+                },
+                {
+                    id:4,
+                    curso:'HTML5-1',
+                },
+                {
+                    id:5,
+                    curso:'HTML5-2',
+                },
+                {
+                    id:6,
+                    curso:'CSS3-1',
+                },
+                {
+                    id:7,
+                    curso:'CSS3-2',
+                },
+            ]
+        },
+        {
+            id:7,
+            institucion:'Vivelab(Universidad Nacional de Colombia)',
+            curso:[
+                {
+                    id:1,
+                    curso:'Usabilidad',
+                },
+                {
+                    id:2,
+                    curso:'Lean y UX',
+                },
+            ]
+        },
+        {
+            id:8,
+            institucion:'Platzi',
+            curso:[
+                {
+                    id:1,
+                    curso:'Diseño de interfaces y Ux',
+                },
+                {
+                    id:2,
+                    curso:'Diseño de interfaces móviles',
+                },
+                {
+                    id:3,
+                    curso:'Diseño de interfaces web',
+                },
+                {
+                    id:4,
+                    curso:'Programación básica',
+                },
+                {
+                    id:5,
+                    curso:'ilustrator',
+                },
+            ]
+        },
+        {
+            id:9,
+            institucion:'Universidad de Artes y Letras',
+            curso:[
+                {
+                    id:1,
+                    curso:'Enfasis en diseño y desarrollo Web',
+                },
+                {
+                    id:2,
+                    curso2:'Técnico Profesional en Diseño Gráfico',
+                }
+            ]
+        },
+        {
+            id:10,
+            institucion:'Colegio Jorge Eliecer Gaitan',
+            curso:[
+                {
+                    id:1,
+                    curso1:'Bachiller académico',
+                },
+            ]
+        },
+    ]
+};
+
+export default InfoEducation;
