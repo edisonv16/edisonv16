75b7f5b49213871d3cb789dbbcbca33c95bbb472
Edison Ospina <edisonv16@hotmail.com>
2026-03-13 23:14:05 -0500

    Add Header greeting tests with Date mocking

--- edison/src/components/header/Header.test.jsx
+++ edison/src/components/header/Header.test.jsx
@@ -7,7 +7,7 @@ const REAL_DATE = Date;
 const mockDate = (isoDate) => {
   global.Date = class extends REAL_DATE {
     constructor(...args) {
-      if (args.length) {
+      if (args.length > 0) {
         return new REAL_DATE(...args);
       }
 
@@ -24,35 +24,40 @@ const restoreDate = () => {
   global.Date = REAL_DATE;
 };
 
+const runGreetingScenario = async ({ isoDate, greeting, className }) => {
+  mockDate(isoDate);
+  render(<Header />);
+
+  expect(await screen.findByText(greeting)).toBeInTheDocument();
+  expect(document.querySelector('#home')).toHaveClass(className);
+};
+
 describe('Header', () => {
   afterEach(() => {
     restoreDate();
   });
 
   test('renders morning greeting with morning class', async () => {
-    mockDate('2024-01-10T09:00:00');
-
-    render(<Header />);
-
-    expect(await screen.findByText(/Buenos Días/i)).toBeInTheDocument();
-    expect(document.querySelector('#home')).toHaveClass('estilo-manana');
+    await runGreetingScenario({
+      isoDate: '2024-01-10T09:00:00',
+      greeting: 'Buenos Días',
+      className: 'estilo-manana',
+    });
   });
 
   test('renders afternoon greeting with afternoon class', async () => {
-    mockDate('2024-01-10T15:00:00');
-
-    render(<Header />);
-
-    expect(await screen.findByText(/Buenas Tardes/i)).toBeInTheDocument();
-    expect(document.querySelector('#home')).toHaveClass('estilo-tarde');
+    await runGreetingScenario({
+      isoDate: '2024-01-10T15:00:00',
+      greeting: 'Buenas Tardes',
+      className: 'estilo-tarde',
+    });
   });
 
   test('renders evening greeting with evening class', async () => {
-    mockDate('2024-01-10T21:00:00');
-
-    render(<Header />);
-
-    expect(await screen.findByText(/Buenas Noches/i)).toBeInTheDocument();
-    expect(document.querySelector('#home')).toHaveClass('estilo-noche');
+    await runGreetingScenario({
+      isoDate: '2024-01-10T21:00:00',
+      greeting: 'Buenas Noches',
+      className: 'estilo-noche',
+    });
   });
 });
